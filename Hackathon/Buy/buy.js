9cc0d51a692dccf4b6e4db5927d61a883390699e
AngkushTarachand <angkushtarachand@gmail.com>
2022-06-28 23:51:19 +0400

    Merge remote-tracking branch 'origin/main'

--- Hackathon/Buy/buy.js
+++ Hackathon/Buy/buy.js
@@ -3,10 +3,80 @@ console.log("Product");
 
 console.log(document.getElementById("AddButton"));
 let element = document.getElementById("AddButton");
-element.addEventListener("click", Add);
+//element.addEventListener("click", Add);
 
 
-function Add(){
-    let sum++
-    return sum
-}
+let plus= document.querySelector(".plus"),
+    minus = document.querySelector(".minus")
+
+    num = document.querySelector(".num");
+   let a = 0;
+   let SumTicket = 0 ;
+   plus.addEventListener("click", ()=>{
+        a++;
+        a = (a < 10 ) ? "0" + a : a ;
+        num.innerText = a ;
+        console.log(a);
+         SumTicket = a*100 ;
+         console.log(document.getElementById("total")) ;
+         let element1 =  document.getElementById("total") ;
+         element1.innerText = SumTicket ;
+         console.log(SumTicket);
+   });
+
+   minus.addEventListener("click", ()=>{
+        a--;
+        a = (a < 10 ) ? "0" + a : a ;
+
+        if (a > 0 ){
+            num.innerText = a ;
+            console.log(a);
+        }else {
+                a=0;
+        }
+         SumTicket = a*100 ;
+         console.log(document.getElementById("total")) ;
+         let element1 =  document.getElementById("total") ;
+         element1.innerText = SumTicket ;
+         console.log(SumTicket);
+   });
+
+   SumTicket = a*100 ;
+   console.log(document.getElementById("total")) ;
+   let element1 =  document.getElementById("total") ;
+   element1.innerText = SumTicket ;
+
+let cart = document.querySelector(".cart");
+    cart.addEventListener("click", ()=> {
+    console.log("adding to cart");
+    let table = document.createElement("table");
+    let tr = document.createElement("tr");
+    let td = document.createElement("td");
+
+    table.appendChild(tr);
+    table.appendChild(td);
+    td.innerText = "Phone" ;
+
+    console.log(table);
+
+    });
+
+
+    let table = document.createElement("table");
+    let tableBody = document.createElement("tbody");
+    let tr = document.createElement("tr");
+    let td = document.createElement("td");
+
+    document.appendChild(table);
+    table.appendChild(tableBody);
+    tableBody.appendChild(tr) ;
+    tr.appendChild(td);
+
+    td.innerText = "Phone";
+
+//    let Product[
+//            {id: 1, name: , numOfTicket: a ,sum: SumTicket },
+//            {id: 2, name:}
+//      ];
+//
+
