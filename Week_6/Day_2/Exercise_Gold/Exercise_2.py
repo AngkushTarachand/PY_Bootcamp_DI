944a05ad88669e8f7b6fe4a320b1e5bd74594b18
AngkushTarachand <angkushtarachand@gmail.com>
2022-07-17 16:48:00 +0400

    Merge remote-tracking branch 'origin/main'

---
+++ Week_6/Day_2/Exercise_Gold/Exercise_2.py
@@ -0,0 +1,12 @@
+print("Week 6 Day 2 Exercise Gold")
+
+# ------------------- Exercise 2 ------------------ #
+month = int(input("Enter a month number( 1 to 12): "))
+if 3 <= month <= 5:
+    print("Spring runs from March(3) to May(5)")
+elif 6 <= month <= 8:
+    print("Summer runs from June(6) to August(8)")
+elif 9 <= month <= 11:
+    print("Autumn runs from September (9) to November (11)")
+elif 0 < month < 3 or month is 12:
+    print("Winter runs from December(12) to February(2)")
