0e8bd55c4e71b47d4c8121c96c4d5c72676d54ef
AngkushTarachand <angkushtarachand@gmail.com>
2022-07-17 16:47:11 +0400

    Merge remote-tracking branch 'origin/main'

---
+++ Week_6/Day_2/Exercise_Gold/Exercise_1.py
@@ -0,0 +1,5 @@
+print("Week 6 Day 2 Exercise Gold")
+
+# ------------------ Exercise 1 ------------------ #
+print("Hello World \n Hello World \n Hello World \n World\n"
+      "I love python\n I love python \n I love python \n I love python")
