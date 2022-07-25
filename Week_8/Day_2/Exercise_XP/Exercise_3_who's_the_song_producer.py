0aec0495247b1a4b4a72e1f9be91ec239f1d2741
AngkushTarachand <angkushtarachand@gmail.com>
2022-07-25 14:00:43 +0400

    Merge remote-tracking branch 'origin/main'

---
+++ Week_8/Day_2/Exercise_XP/Exercise_3_who's_the_song_producer.py
@@ -0,0 +1,13 @@
+class Song:
+    def __init__(self, lyrics):
+        self.lyrics = lyrics
+
+    def sing_me_a_song(self):
+        for each in self.lyrics:
+            print(each)
+
+
+stairway = Song(["There’s a lady who's sure",
+                 "all that glitters is gold",
+                 "and she’s buying a stairway to heaven"])
+stairway.sing_me_a_song()
